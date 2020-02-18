4b2ef81397b56a78b4a07a61eedc5a99bb6362c9
patkhai <patrick.phyo@gmail.com>
2020-02-18 03:42:30 -0800

    weather-forcast application

---
+++ src/components/Body.vue
@@ -0,0 +1,36 @@
+<template>
+    <body class="Body">
+    <h1>Weather Forecast</h1>
+    </body>
+</template>
+
+
+<script>
+export default {
+    name: "Body"
+}
+</script>
+
+
+<style scoped>
+
+h1 { 
+  text-align: center; 
+  font-weight: 800;
+  font-size: 50px;
+  color: rgb(14, 13, 11);
+  padding: 15px;
+  margin: 30px;
+  margin-bottom: 30px;
+
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+
+  text-shadow: 3px 5px rgba(0, 0, 0, 0.11);
+  border-radius: 16px;
+
+}
+
+</style>
